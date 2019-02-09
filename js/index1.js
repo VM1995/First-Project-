b73e6da743d73575d823d7bee78f7b8de1dad14c
Vitalja Misnjankin <47272421+VM1995@users.noreply.github.com>
2019-02-09 23:08:15 +0300

    firth comit

---
+++ js/index1.js
@@ -0,0 +1,10 @@
+function srav(){    
+     var login = document.getElementById("login").value;
+      var password = document.getElementById("password").value;
+  
+   if (login ==  123 && password == 123) 
+      location.href = "Index2.html";
+    else {
+       alert('А вот и неправильно!'); 
+     }
+}
